d7265501aab3865b681c3da31802715675e3704a
coltenc <cc1027210@otc.edu>
2025-10-01 09:47:07 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+int row = 12;
+int column = 12;
+
+for (int x = 1; x <= row; x++)
+{
+    for (int y = 1; y <= column; y++)
+    {
+        int z = x * y;
+        Console.Write($"|{z}|" + "    ");
+        if (y == 12)
+        {
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------------------------------------------------------------");
+        }
+    }
+    Console.WriteLine();
+}
